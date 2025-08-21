9e6fdd7b075b0ea81cc670315188d6e4ce232178
akhilth32-lang <akhilth3.2@gmail.com>
2025-08-21 14:50:12 +0530

    Create countries.py

---
+++ config/countries.py
@@ -0,0 +1,67 @@
+# config/countries.py
+
+COUNTRIES = [
+    {
+        "id": "global",
+        "name": "Global",
+        "countryCode": "global",
+        "isCountry": False
+    },
+    {
+        "id": 32000006,
+        "name": "International",
+        "isCountry": False
+    },
+    {
+        "id": 32000249,
+        "name": "United States",
+        "isCountry": True,
+        "countryCode": "US"
+    },
+    {
+        "id": 32000248,
+        "name": "United Kingdom",
+        "isCountry": True,
+        "countryCode": "GB"
+    },
+    {
+        "id": 32000094,
+        "name": "Germany",
+        "isCountry": True,
+        "countryCode": "DE"
+    },
+    {
+        "id": 32000087,
+        "name": "France",
+        "isCountry": True,
+        "countryCode": "FR"
+    },
+    {
+        "id": 32000113,
+        "name": "India",
+        "isCountry": True,
+        "countryCode": "IN"
+    },
+    {
+        "id": 32000166,
+        "name": "Netherlands",
+        "isCountry": True,
+        "countryCode": "NL"
+    },
+    {
+        "id": 32000000,
+        "name": "Europe",
+        "isCountry": False
+    },
+    {
+        "id": 32000001,
+        "name": "North America",
+        "isCountry": False
+    },
+    {
+        "id": 32000002,
+        "name": "South America",
+        "isCountry": False
+    },
+    # Add more countries or regions as necessary
+]
