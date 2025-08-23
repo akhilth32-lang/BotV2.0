beddbcc4007268251b2a88f6b7bc6e26ea3c31a4
akhilth32-lang <akhilth3.2@gmail.com>
2025-08-23 21:08:02 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,5 @@
+# keep_alive.py
+
 from flask import Flask
 from threading import Thread
 
@@ -11,6 +13,6 @@ def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    server = Thread(target=run)
+    server.start()
     
