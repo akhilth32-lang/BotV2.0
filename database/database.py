03e1929976e2673dcf9ebd94d33b64d2be9df4b4
akhilth32-lang <akhilth3.2@gmail.com>
2025-08-23 16:37:13 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,18 +1,9 @@
-# database/database.py
-import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
+from config.settings import MONGO_URI
 
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI")
 mongo_client = MongoClient(MONGO_URI)
-db = mongo_client["coc_bot"]
+db = mongo_client['clash_bot']
 
-players_col = db["players"]
-backup_col = db["backup_players"]
-
-# Ensure indexes for performance
-players_col.create_index([("trophies", -1)])
-players_col.create_index([("player_tag", 1)])
-players_col.create_index([("discord_id", 1)])
+def get_collection(collection_name: str):
+    return db[collection_name]
+  
