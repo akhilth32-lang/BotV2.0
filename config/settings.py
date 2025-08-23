ce5fb05a48d6051ca8d2dc4f0fb0ad4e3fc18dd6
akhilth32-lang <akhilth3.2@gmail.com>
2025-08-23 20:31:40 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,16 +1,7 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+COC_EMAIL = os.getenv("COC_EMAIL")
+COC_PASS = os.getenv("COC_PASS")
 
-# MongoDB connection string from environment variable
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-
-# Discord Bot Token
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN", "")
-
-# Clash of Clans API token
-COC_API_TOKEN = os.getenv("COC_API_TOKEN", "")
-
-# Other settings
-BOT_PREFIX = "!"
+MONGO_URI = os.getenv("MONGO_URI")
